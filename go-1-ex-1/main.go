772a75675a1c294960373c565628248af6db1725
VORNAME NACHNAME <VORNAME_NACHNAME@sluz.ch>
2024-11-21 14:44:35 +0100

    Hier meine bearbeiteten Aufgaben 1 - 3 des ersten Blocks.

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Mika"
+	lastName := "Blaettler"
+	dayOfBirth := 3
+	monthOfBirth := 7
+	yearOfBirth := 2008
+	numberOfSiblings := 2
+	heightInMeters := 1.75
+	zodiacSign := '\u264b'
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -11,11 +12,13 @@ func main() {
 	var when = time.Now()
 
 	// TODO: use fmt.Fprintln instead!
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
 	fmt.Println("the dice shows", eyes, "eyes")
 
 	// TODO: use fmt.Fprintln instead!
+	fmt.Fprintln(os.Stderr, "The dice was rolled at", when)
 	fmt.Println("the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+	// go run ex3/main.go >eyes.txt 2>dice.log
 }
